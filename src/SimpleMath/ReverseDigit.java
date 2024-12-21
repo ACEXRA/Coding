d8633fb94a08906de543c556eeb8c72a41515acd
ACExRA <rasheed.786ahamed@gmail.com>
2024-12-22 00:12:13 +0530

    Coding problems

--- src/SimpleMath/ReverseDigit.java
+++ src/SimpleMath/ReverseDigit.java
@@ -1,15 +1,16 @@
-package SimpleMath;
+package src.SimpleMath;
 
 public class ReverseDigit {
     public static void main(String[] args) {
-        System.out.println(reverseDigit(256789));
+        System.out.println(reverseDigit(256000));
     }
-    public static long reverseDigit(long n){
-        long rev=0;
-        while(n>0){
-            int rem=(int)n%10;
-            rev=rev*10+rem;
-            n/=10;
+
+    public static long reverseDigit(long n) {
+        long rev = 0;
+        while (n > 0) {
+            int temp = (int) n % 10;
+            rev = rev * 10 + temp;
+            n /= 10;
         }
         return rev;
     }
